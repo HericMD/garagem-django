5940d7b94bc898608b3b6b61ff0e6cb700b50f30
HericMD <hericmdms@gmail.com>
2023-03-31 10:29:11 -0300

    mais modelos

--- garagem/models.py
+++ garagem/models.py
@@ -12,3 +12,35 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.descricao
+
+class Acessorio(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+
+    class Meta:
+        verbose_name_plural = "Acessórios"
+
+class Cor(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+
+    class Meta:
+        verbose_name_plural = "Cores"
+
+class Veiculo(models.Model):
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="marca")
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="categoria")
+    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="cor")
+    ano = models.IntegerField(blank=True, null=True, default=0)
+    preco = models.DecimalField(max_digits=10, decimal_places = 2, blank=True, null=True, default=0)
+
+    def __str__(self):
+        return f"{self.marca} {self.categoria} {self.ano} {self.cor}"
+    
+    class Meta:
+        verbose_name_plural = "veículos"
+        verbose_name = "veículo"
