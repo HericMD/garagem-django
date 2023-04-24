01bdd16e5bb919bb5d22e89f7c802464d0560195
HericMD <hericmdms@gmail.com>
2023-04-24 16:01:57 -0300

    API REST

---
+++ garagem/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework.serializers import ModelSerializer
+
+from garagem.models import Acessorio, Categoria, Cor, Veiculo
+
+class AcessorioSerializer(ModelSerializer):
+    class Meta:
+        model = Acessorio
+        fields = "__all__"
+
+class CategoriaSerializer(ModelSerializer):
+    class Meta:
+        model = Categoria
+        fields = "__all__"
+
+class CorSerializer(ModelSerializer):
+    class Meta:
+        model = Cor
+        fields = "__all__"
+    
+class VeiculoSerializer(ModelSerializer):
+    class Meta:
+        model = Veiculo
+        fields = "__all__"
--- garagem/views.py
+++ garagem/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+
+from garagem.models import Acessorio, Categoria, Cor, Veiculo
+from garagem.serializers import AcessorioSerializer, CategoriaSerializer, CorSerializer, VeiculoSerializer
+
+class AcessorioViewSet(ModelViewSet):
+    queryset = Acessorio.objects.all()
+    serializer_class = AcessorioSerializer
+
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+
+class CorViewSet(ModelViewSet):
+    queryset = Cor.objects.all()
+    serializer_class = CorSerializer
+
+class VeiculoViewSet(ModelViewSet):
+    queryset = Veiculo.objects.all()
+    serializer_class = VeiculoSerializer
