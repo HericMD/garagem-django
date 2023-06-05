1a0bf04d3d6a06d13d3ad1866404e1c4b8ae7045
HericMD <hericmdms@gmail.com>
2023-06-05 16:16:03 -0300

    Relactionamento n para n (acessórios)

--- garagem/models.py
+++ garagem/models.py
@@ -38,15 +38,16 @@ class Marca(models.Model):
 
 class Veiculo(models.Model):
     nome = models.CharField(max_length=50)
-    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="marca")
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="veiculos")
     categoria = models.ForeignKey(
-        Categoria, on_delete=models.PROTECT, related_name="categoria"
+        Categoria, on_delete=models.PROTECT, related_name="veiculos"
     )
-    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="cor")
+    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculos")
     ano = models.IntegerField(blank=True, null=True, default=0)
     preco = models.DecimalField(
         max_digits=10, decimal_places=2, blank=True, null=True, default=0
     )
+    acessorios = models.ManyToManyField(Acessorio, related_name="veiculos")
 
     def __str__(self):
         return f"{self.marca} {self.categoria} {self.ano} {self.cor}"
